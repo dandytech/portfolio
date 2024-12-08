19b58dee2c9f235c9eaed891035dd3240c018ab5
dandytech <dannkwo@gmail.com>
2024-12-08 08:14:19 +0100

    my first commit

---
+++ JavaScriptTest/mydateFunctionScript.js
@@ -0,0 +1,4 @@
+function myDateFunction() {
+    let d = Date();
+    window.alert("Today's Date is: " +d);
+}
---
+++ JavaScriptTest/person.js
@@ -0,0 +1,13 @@
+//You can Export using Named or Default export
+
+// using named export
+export const name = "Obi Nweke";
+export const age = 30;
+export const state = "Lagos"
+
+/* 
+Using default Export
+const name = "Daniel";
+const age = 40;
+export {name, age};
+*/
